149d8d6c0f2cc872bce5d7dbd7725a1fc469bd5a
Aditya Kolla <aditya.169108011@muj.manipal.edu>
2019-02-07 00:53:15 +0530

    Updates

---
+++ precache-manifest.5d4f8257433fc797c53f0e949e6d134c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8df80dd3505eb1858b49327acd63dcbb",
+    "url": "/rockpaper-game/static/media/scissor.8df80dd3.png"
+  },
+  {
+    "revision": "4dcc12c0f8dddc9d8f72f159bb012bc0",
+    "url": "/rockpaper-game/static/media/rock.4dcc12c0.png"
+  },
+  {
+    "revision": "1ef92cf2b60d7f69f1d00efdd8c8f9f9",
+    "url": "/rockpaper-game/static/media/paper.1ef92cf2.png"
+  },
+  {
+    "revision": "afac6affc332673a6597",
+    "url": "/rockpaper-game/static/js/runtime~main.afac6aff.js"
+  },
+  {
+    "revision": "91bff2e87cfd9b48199d",
+    "url": "/rockpaper-game/static/js/main.91bff2e8.chunk.js"
+  },
+  {
+    "revision": "ac52dc1e9c911371b131",
+    "url": "/rockpaper-game/static/js/1.ac52dc1e.chunk.js"
+  },
+  {
+    "revision": "91bff2e87cfd9b48199d",
+    "url": "/rockpaper-game/static/css/main.00221c8e.chunk.css"
+  },
+  {
+    "revision": "4b432d418d07534cb2d1ac3a0ba11b32",
+    "url": "/rockpaper-game/index.html"
+  }
+];
